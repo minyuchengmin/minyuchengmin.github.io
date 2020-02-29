17b18b14d7ca3f0c07195c11de218d5151bca813
minyuchengmin <minyuchengmin@outlook.com>
2020-02-29 09:39:44 +0800

    Site updated: 2020-02-29 09:39:42

---
+++ dist/music.js
@@ -0,0 +1,37 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "Alone",
+        artist: 'Alan Walker',
+        url: 'http://minyuchengmin.github.io/music/Alan%20Walker%20-%20Alone.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003ArgKV3mjzwp.jpg?max_age=2592000',
+      },
+      {
+        name: 'Faded',
+        artist: 'Alan Walker',
+        url: 'http://minyuchengmin.github.io/music/Alan%20Walker%20-%20Fade.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002Nt51E0q8Zoo.jpg?max_age=2592000',
+      },
+      {
+        name: 'Force',
+        artist: 'Alan Walker',
+        url: 'http://minyuchengmin.github.io/music/Alan%20Walker%20-%20Force.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003rpu1B3gZbNT.jpg?max_age=2592000',
+      },
+      {
+        name: 'Interlude',
+        artist: 'Alan Walker',
+        url: 'http://minyuchengmin.github.io/music/Alan%20Walker%20-%20Interlude.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000049de8x3025G8.jpg?max_age=2592000',
+      },
+      {
+        name: 'Intro',
+        artist: 'Alan Walker',
+        url: 'http://minyuchengmin.github.io/music/Alan%20Walker%20-%20Intro.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000049de8x3025G8.jpg?max_age=2592000',
+      },
+    ]
+});
